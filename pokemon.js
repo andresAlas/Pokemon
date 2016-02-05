e45ab61f1015a74fdddd4614e820b7ec710dc7f9
andres <andres.alas@hotmail.com>
2016-02-05 12:37:53 -0600

    Termine el codigo

--- pokemon.js
+++ pokemon.js
@@ -14,10 +14,14 @@ function inicio()
 {
 	var pikachu = new pokemon("Pikachu",50,22);
 	var totodile = new pokemon("Totodile",52,20);
+	var treecko = new pokemon("Treecko",60,30);
+	var torchic = new pokemon("Torchic",55,27);
 	pikachu.grito = "Pikaaa!";
 	totodile.grito = "Tototodilee!";
+	treecko.grito = "Treeckoooo!";
+	torchic.grito = "Torchiiiic!";
 
-	var elija = prompt("Elija un Pokemon: \n Pikachu: 0 \n Totodile: 1", 0);
+	var elija = prompt("Elija un Pokemon: \n Pikachu: 0 \n Totodile: 1 \n Treecko: 2 \n Torchic: 3", 0);
 
 	if(elija == 0) 
 	{
@@ -33,5 +37,21 @@ function inicio()
 		datosPokemon.textContent = "La vida del pokemon es: " + totodile.vida + " El ataque del pokemon es: " + totodile.ataque + " Su grito es: " + totodile.grito;
 		document.write("<img src=imagenes/totodile.png>");
 		totodile.gritar();
+	}
+
+	if(elija == 2)
+	{
+		nombrePokemon.textContent = treecko.nombre;
+		datosPokemon.textContent = "La vida del pokemon es: " + treecko.vida + " El ataque del pokemon es: " + treecko.ataque + " Su grito es: " + treecko.grito;
+		document.write("<img src=imagenes/treecko.gif>");
+		treecko.gritar();
+	}
+
+	if(elija == 3)
+	{
+		nombrePokemon.textContent = torchic.nombre;
+		datosPokemon.textContent = "La vida del pokemon es: " + torchic.vida + " El ataque del pokemon es: " + torchic.ataque + " Su grito es: " + torchic.grito;
+		document.write("<img src=imagenes/torchic.gif>");
+		torchic.gritar();
 	}
 }
