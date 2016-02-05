d504a0a5465827d38433561cea1fce291edabb32
andres <andres.alas@hotmail.com>
2016-02-05 09:14:41 -0600

    Errores en el codigo

--- pokemon.js
+++ pokemon.js
@@ -1,6 +1,6 @@
-function pokemon(n,v,a)
+function pokemon(n,v,a,g)
 {
-	this.grito = "Pika";
+	this.grito = g;
 	this.nombre = n;
 	this.vida = v;
 	this.ataque = a;
@@ -12,7 +12,15 @@ function pokemon(n,v,a)
 
 function inicio()
 {
-	var pikachu = new pokemon("Pikachu", 50, 25);
-	nombrePokemon.textContent = pikachu.nombre;
-	datosPokemon.textContent = pikachu.ataque;
+	var opcionNombre = ["Pikachu","Totodile"];
+	var opcionGrito = ["Pikaaa","Tototodilee"];
+	opcionUsuario = prompt("Que Pokemon eliges: \n Pikachu: 0 \n Totodile: 1", 0);
+
+	if(opcionNombre[opcionUsuario] == Pikachu)
+	{
+		var opcionPokemon = new pokemon(opcionNombre[opcionUsuario], 50, 25, opcionGrito[opcionUsuario]);
+		nombrePokemon.innerText = opcionPokemon.nombre;
+		document.write("<img src=imagenes/pikachu.png/>");
+		datosPokemon.innerText = "El nombre del Pokemon es: " + opcionPokemon.nombre + "La vida del Pokemon es: " + opcionPokemon.vida + "El ataque del Pokemon es: " + opcionPokemon.ataque;
+	}
 }
